ba86b899d60bb92e7190e9d5fa5b69e2c20e320b
Samuel Henry <samuelhenry@gmail.com>
2013-03-02 14:41:29 -0600

    Initial Commit

---
+++ Friendly PlacesTests/Friendly_PlacesTests.h
@@ -0,0 +1,13 @@
+//
+//  Friendly_PlacesTests.h
+//  Friendly PlacesTests
+//
+//  Created by Sam on 3/2/13.
+//  Copyright (c) 2013 sam henry. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Friendly_PlacesTests : SenTestCase
+
+@end
---
+++ Friendly PlacesTests/Friendly_PlacesTests.m
@@ -0,0 +1,32 @@
+//
+//  Friendly_PlacesTests.m
+//  Friendly PlacesTests
+//
+//  Created by Sam on 3/2/13.
+//  Copyright (c) 2013 sam henry. All rights reserved.
+//
+
+#import "Friendly_PlacesTests.h"
+
+@implementation Friendly_PlacesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Friendly PlacesTests");
+}
+
+@end
